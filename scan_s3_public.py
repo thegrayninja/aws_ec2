20912ac33713bc29a9e63da7918f721bf3501cd7
thegrayninja <grayninja@protonmail.com>
2018-07-25 16:29:33 -0700

    Update scan_s3_public.py

--- scan_s3_public.py
+++ scan_s3_public.py
@@ -1,21 +1,24 @@
 import requests
 
 import xml.etree.ElementTree as etree
+import os
+import subprocess
+import json
 
 
 
 def main():
 
-    OpenBucketList()
-
+    BucketList = OpenBucketList()
+    #AccessBucketURL(BucketList)
+    AccessBucketAWSCLI(BucketList)
 
 
 def OpenBucketList():
     with open("bucket_list.txt", "r") as BucketFile:
         Buckets = BucketFile.readlines()
 
-    AccessBucketURL(Buckets)
-    return 0
+    return Buckets
 
 
 def AccessBucketURL(Buckets):
@@ -36,12 +39,39 @@ def GetXMLIfValid(Bucket, Response):
     tree = etree.fromstring(Response.content)
     node = tree.find('Message')
     print("{} - {} - {}".format(Bucket, tree.tag, node.text))
-    #print(node.text)
-    # print(URLStatus.xpath('//Error'))
+
 
 
 def GetPageContentIfValid(Bucket, Response):
     print("{} - success?".format(Bucket))
+
+
+
+def AccessBucketAWSCLI(Buckets):
+    Success = []
+    Failure = []
+    Commands = []
+    for i in Buckets:
+        Bucket = i.strip()
+        Profile = "backup"
+        Command = ['aws', '--profile', '{}'.format(Profile), 's3api', 'list-objects', '--bucket', '{}'.format(Bucket)]#.format(Profile, Bucket)
+
+        Result = subprocess.Popen(Command, stdout=subprocess.PIPE)
+        Output = Result.stdout.read().decode("utf-8")
+        print(Output)
+        try:
+            if Output[0] == "{":
+                Success.append(Bucket)
+        except:
+
+            Failure.append(Bucket)
+        Commands.append(Bucket)
+
+    print("\n\n\n")
+    print("Successful Attempts - Open Access:")
+    print(Success)
+    print("Failed Attempts - Access Denied:")
+    print(Failure)
 
 
 if __name__ == main():
