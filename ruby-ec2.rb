e5331853a4902c803a48a7513a5be8f4ac9c7cf6
grayninja <grayninja@protonmail.com>
2018-10-24 22:33:48 -0700

    update aws

--- ruby-ec2.rb
+++ ruby-ec2.rb
@@ -12,7 +12,7 @@ def Main()
   ec2 = SetupEC2Instance()
   data = GetInstanceInfo(ec2)
   #data = "data_not_accurate"
-  ChangeEC2State(data)
+  ChangeEC2State(data, ec2)
   puts "i hope you are satisfied"
 end
 
@@ -49,7 +49,7 @@ def GetInstanceInfo(ec2)
 end
 
 
-def ChangeEC2State(instance_data)
+def ChangeEC2State(instance_data, ec2)
   puts "Would you like to start up a machine? [y/n]"
   print "> "
   user_response_start = $stdin.gets.chomp
@@ -62,7 +62,77 @@ def ChangeEC2State(instance_data)
       puts "You fool, that's a character...not a number! Try again."
       ChangeEC2State(instance_data)
     else
-      puts "Starting Instance: #{instance_data[user_response_id]}"
+      instance_id = instance_data[user_response_id]
+      puts "Starting Instance: #{instance_id}"
+      StartEC2Instance(instance_id, ec2)
+      puts "Would you like to start/stop another instance? [y/n]"
+      print "> "
+      user_response_change = $stdin.gets.chomp
+      if user_response_change == "y"
+        ChangeEC2State(instance_data, ec2)
+      else
+        return 0
+      end
+    end
+  end
+
+  puts "Would you like to STOP a machine? [y/n]"
+  print "> "
+  user_response_stop = $stdin.gets.chomp
+
+  if user_response_stop = "y"
+    puts "Please enter the row number you'd like to STOP [ie, 1]"
+    print "> "
+    user_response_id = $stdin.gets.chomp.to_i
+    if user_response_id == 0
+      puts "You fool, that's a character...not a number! Try again."
+      ChangeEC2State(instance_data)
+    else
+      instance_id = instance_data[user_response_id]
+      puts "Stopping Instance: #{instance_id}"
+      StopEC2Instance(instance_id, ec2)
+      puts "Would you like to start/stop another instance? [y/n]"
+      print "> "
+      user_response_change = $stdin.gets.chomp
+      if user_response_change == "y"
+        ChangeEC2State(instance_data, ec2)
+      else
+        return 0
+      end
+    end
+  end
+end
+
+
+def StartEC2Instance(instance_id, ec2)
+  i = ec2.instance(instance_id)
+  if i.exists?
+    case i.state.code
+    when 0  # pending
+      puts "#{instance_id} is pending, so it will be running in a bit"
+    when 16  # started
+      puts "#{instance_id} is already started"
+    when 48  # terminated
+      puts "#{instance_id} is terminated, so you cannot start it"
+    else
+      i.start
+    end
+  end
+end
+
+
+def StopEC2Instance(instance_id, ec2)
+  i = ec2.instance(instance_id)
+  if i.exists?
+    case i.state.code
+    when 48  # terminated
+      puts "#{instance_id} is terminated, so you cannot stop it"
+    when 64  # stopping
+      puts "#{instance_id} is stopping, so it will be stopped in a bit"
+    when 80  # stopped
+      puts "#{instance_id} is already stopped"
+    else
+      i.stop
     end
   end
 end
