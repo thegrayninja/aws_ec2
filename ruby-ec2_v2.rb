e0019ab7152f2b5d837b2f43d55c43216d2e9b1a
grayninja <grayninja@protonmail.com>
2018-11-08 21:46:45 -0800

    updating

--- ruby-ec2_v2.rb
+++ ruby-ec2_v2.rb
@@ -55,8 +55,6 @@ class GetInstanceData
 
       @counter += 1
     end
-    #puts instance_data
-    #puts ec2InstanceList
   end
 
 end
@@ -65,71 +63,54 @@ class EC2
   def initialize(instance_id, connString)
     @instance_id = instance_id
     @ec2 = connString
+
+    @statusCodes = Hash.new
+    @statusCodes[0] = "pending"
+    @statusCodes[16] = "running"
+    @statusCodes[48] = "terminated"
+    @statusCodes[64] = "stopping"
+    @statusCodes[80] = "stopped"
   end
 
-  # attr_reader :instance_id
-  # attr_reader :ec2connstring
 
   def start
-    #@instance_id = instance_id
-    #@ec2 = ec2
     id = @ec2.instance(@instance_id)
     if id.exists?
-      case id.state.code
-      when 0  # pending
-        puts "#{@instance_id} is pending, so it will be running in a bit"
-      when 16  # started
-        puts "#{@instance_id} has already started"
-      when 48  # terminated
-        puts "#{@instance_id} is terminated, so you cannot start it"
-      else
-        id.start
-      end
+      id.start
+      statusCode = @statusCodes[id.state.code]
+      puts "#{@instance_id} - #{statusCode}"
+    else
+      puts "ERROR - cannot find #{instance_id}"
     end
   end
 
+
   def stop
-    #@instance_id = instance_id
-    #@ec2 = ec2
     id = @ec2.instance(@instance_id)
     if id.exists?
-      case id.state.code
-      when 48  # terminated
-        puts "#{@instance_id} is terminated, so you cannot stop it"
-      when 64  # stopping
-        puts "#{@instance_id} is stopping, so it will be stopped in a bit"
-      when 80  # stopped
-        puts "#{@instance_id} is already stopped"
-      else
-        id.stop
-      end
+      id.stop
+      statusCode = @statusCodes[id.state.code]
+      puts "#{@instance_id} - #{statusCode}"
+    else
+      puts "ERROR - cannot find #{instance_id}"
     end
   end
 
   def state
-    #@instance_id = instance_id
-    #@ec2 = ec2
-    puts @instance_id
+    #puts @instance_id
     id = @ec2.instance(@instance_id)
     if id.exists?
-      case id.state
-      when 0  # pending
-        puts "#{@instance_id} is pending, so it will be running in a bit"
-      when 16  # started
-        puts "#{@instance_id} has already started"
-      when 48  # terminated
-        puts "#{@instance_id} is terminated, so you cannot stop it"
-      when 64  # stopping
-        puts "#{@instance_id} is stopping, so it will be stopped in a bit"
-      when 80  # stopped
-        puts "#{@instance_id} is stopped"
-      else
-        puts id.state
-      end
+      statusCode = @statusCodes[id.state.code]
+      puts "#{@instance_id} - #{statusCode}"
+    else
+      puts "ERROR - cannot find #{instance_id}"
     end
   end
 
 end
+
+
+
 
 
 def RunProgram()
@@ -140,36 +121,22 @@ def RunProgram()
   myEc2Instances = GetInstanceData.new(connString)
   myEc2Instances.info
   instances = myEc2Instances.instance_data
-  puts instances
+
+  puts "\ntesting values:"
+  instances.values.each do |instance|
+    puts "suck it #{instance}"
+  end
+  puts "\ntesting collect:"
+  puts instances.collect { |k, v| v }
+
+  #puts instances
   test1 = instances[1]
-  puts "\n#{test1}"
+  #puts "\n#{test1}"
 
   testinstance1 = EC2.new(test1, connString)
   puts testinstance1.state
-  puts testinstance1.stop
-  puts testinstance1.state
-  # puts "\nWould you like to power ON an instance? [y/n]"
-  # print "> "
-  # response = $stdin.gets.chomp()
-  # puts response
-  # if response == "y"
-  #   puts "Please enter number associated with instance:"
-  #   print("> ")
-  #   response = $stdin.gets.chomp()
-  #   instanceID = instances[response.to_i]
-  #   instance_test1 = EC2.new(instances[instanceID], uswest1.awsRegion)
-  #   puts instance_test1.state
-  #   #instanceState = myEc2Instances.state
-  #   #puts instanceState(instances[response.to_i], )
-  # else
-  #   puts "ok, things will stay the same"
-  # end
-  # puts "\nWould you like to power OFF an instance? [y/n]"
-  # print "> "
-  # response = $stdin.gets.chomp()
-  # puts response
-
-  #myEc2Instances.GetInstanceInfo
+  #puts testinstance1.stop
+  #puts testinstance1.state
 
 end
 
@@ -177,136 +144,4 @@ end
 RunProgram()
 
 
-
-# def Main()
-#   puts "thanks for turning me on"
-#   ec2 = SetupEC2Instance()
-#   data = GetInstanceInfo(ec2)
-#   #data = "data_not_accurate"
-#   ChangeEC2State(data, ec2)
-#   puts "i hope you are satisfied"
-# end
-#
-#
-# def SetupEC2Instance()
-#   ec2 = Aws::EC2::Resource.new(region: 'us-west-1')
-#   return ec2
-# end
-
-
-
-# def GetInstanceInfo(ec2)
-#   counter = 1
-#   instance_data = Hash.new
-#   ec2.instances.each do |i|
-#     data = ""
-#     data += "#{counter.to_s} || "
-#     data += "#{i.id} || "
-#     data += "#{i.state.name} || "
-#
-#     data += "#{i.key_name} || "
-#     data += "#{i.public_ip_address} ||"
-#
-#     i.tags.each do |tags|
-#       data += "#{tags['key']}:#{tags['value']} "
-#     end
-#
-#     instance_data[counter] = i.id
-#     puts data
-#     counter += 1
-#   end
-#
-#   return instance_data
-# end
-
-
-# def ChangeEC2State(instance_data, ec2)
-#   puts "Would you like to start up a machine? [y/n]"
-#   print "> "
-#   user_response_start = $stdin.gets.chomp
-#
-#   if user_response_start == "y"
-#     puts "Please enter the row number you'd like to spin up [ie, 1]"
-#     print "> "
-#     user_response_id = $stdin.gets.chomp.to_i
-#     if user_response_id == 0
-#       puts "You fool, that's a character...not a number! Try again."
-#       ChangeEC2State(instance_data)
-#     else
-#       instance_id = instance_data[user_response_id]
-#       puts "Starting Instance: #{instance_id}"
-#       StartEC2Instance(instance_id, ec2)
-#       puts "Would you like to start/stop another instance? [y/n]"
-#       print "> "
-#       user_response_change = $stdin.gets.chomp
-#       if user_response_change == "y"
-#         ChangeEC2State(instance_data, ec2)
-#       else
-#         return 0
-#       end
-#     end
-#   end
-#
-#   puts "Would you like to STOP a machine? [y/n]"
-#   print "> "
-#   user_response_stop = $stdin.gets.chomp
-#
-#   if user_response_stop == "y"
-#     puts "Please enter the row number you'd like to STOP [ie, 1]"
-#     print "> "
-#     user_response_id = $stdin.gets.chomp.to_i
-#     if user_response_id == 0
-#       puts "You fool, that's a character...not a number! Try again."
-#       ChangeEC2State(instance_data)
-#     else
-#       instance_id = instance_data[user_response_id]
-#       puts "Stopping Instance: #{instance_id}"
-#       StopEC2Instance(instance_id, ec2)
-#       puts "Would you like to start/stop another instance? [y/n]"
-#       print "> "
-#       user_response_change = $stdin.gets.chomp
-#       if user_response_change == "y"
-#         ChangeEC2State(instance_data, ec2)
-#       else
-#         return 0
-#       end
-#     end
-#   end
-# end
-#
-#
-# def StartEC2Instance(instance_id, ec2)
-#   i = ec2.instance(instance_id)
-#   if i.exists?
-#     case i.state.code
-#     when 0  # pending
-#       puts "#{instance_id} is pending, so it will be running in a bit"
-#     when 16  # started
-#       puts "#{instance_id} is already started"
-#     when 48  # terminated
-#       puts "#{instance_id} is terminated, so you cannot start it"
-#     else
-#       i.start
-#     end
-#   end
-# end
-#
-#
-# def StopEC2Instance(instance_id, ec2)
-#   i = ec2.instance(instance_id)
-#   if i.exists?
-#     case i.state.code
-#     when 48  # terminated
-#       puts "#{instance_id} is terminated, so you cannot stop it"
-#     when 64  # stopping
-#       puts "#{instance_id} is stopping, so it will be stopped in a bit"
-#     when 80  # stopped
-#       puts "#{instance_id} is already stopped"
-#     else
-#       i.stop
-#     end
-#   end
-# end
-
-
-# Main()
+#if this line shows up on debug list, something isn't closed out
