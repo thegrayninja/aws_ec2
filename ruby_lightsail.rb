13a71f2c1198f9d635c18cf50ba94ad210df45a0
grayninja <grayninja@protonmail.com>
2018-11-15 06:21:24 -0800

    basic firewall updating added

--- ruby_lightsail.rb
+++ ruby_lightsail.rb
@@ -2,53 +2,84 @@ require 'aws-sdk-lightsail'
 # require 'json/add/struct'
 # require 'ostruct'
 
-client = Aws::Lightsail::Client.new(region: 'us-east-1')
+
+class AWSSetup
+  def initialize
+    @client = Aws::Lightsail::Client.new(region: 'us-east-1')
+    #@client = @clientString.get_instances
+  end
+
+  attr_reader :client
+
+end
+
 #client.operations
 #resp = client.get_instances()
-resp = client.get_instances()
 
-iName = resp.instances[0].name
-iPublicIP = resp.instances[0].public_ip_address
-iDateCreated = resp.instances[0].created_at
-iFirewall = resp.instances[0].networking.ports
-iState = resp.instances[0].state.name
+class LightsailStatus
+  def getStatus#(client)
+    #@client = client
+    @client = Aws::Lightsail::Client.new(region: 'us-east-1')
+    resp = @client.get_instances
+
+    iName = resp.instances[0].name
+    iPublicIP = resp.instances[0].public_ip_address
+    iDateCreated = resp.instances[0].created_at
+    iFirewall = resp.instances[0].networking.ports
+    iState = resp.instances[0].state.name
 
 
 
-puts "#{iName}\n\t#{iPublicIP}\n\t#{iDateCreated}\n\t#{iState}"
-iFirewall.each do |i|
-  puts "\n\tfrom: #{i.from_port}\n\tto: #{i.to_port}\n\tprotocol:\n\t#{i.protocol}\n\tfrom: #{i.access_from}\n"
+      puts "#{iName}\n\t#{iPublicIP}\n\t#{iDateCreated}\n\t#{iState}"
+    iFirewall.each do |i|
+      puts "\n\tfrom: #{i.from_port}\n\tto: #{i.to_port}\n\tprotocol:\n\t#{i.protocol}\n\tfrom: #{i.access_from}\n"
+    end
+  end
 end
 
-
-resp = client.put_instance_public_ports({
-  port_infos: [ # required
-    {
-      from_port: 80,
-      to_port: 80,
-      protocol: "tcp", # accepts tcp, all, udp
-    },
-    {
-      from_port: 80,
-      to_port: 80,
-      protocol: "tcp", # accepts tcp, all, udp
-    },
-    {
-      from_port: 22,
-      to_port: 22,
-      protocol: "tcp", # accepts tcp, all, udp
-    },
-  ],
-  instance_name: "Ub-Virginia-1", # required
+class UpdateFirewall
+  def updateFirewall#(client)
+    #@client = client
+    @client = Aws::Lightsail::Client.new(region: 'us-east-1')
+    resp = @client.put_instance_public_ports({
+      port_infos: [ # required
+        {
+          from_port: 80,
+          to_port: 80,
+          protocol: "tcp", # accepts tcp, all, udp
+        },
+        {
+          from_port: 443,
+          to_port: 443,
+          protocol: "tcp", # accepts tcp, all, udp
+        },
+        # {
+        #   from_port: 22,
+        #   to_port: 22,
+        #   protocol: "tcp", # accepts tcp, all, udp
+        # },
+      ],
+      instance_name: "Ub-Virginia-1", # required
 })
-
-resp = client.get_instances()
-iFirewall = resp.instances[0].networking.ports
-
-puts "#{iName}\n\t#{iPublicIP}\n\t#{iDateCreated}\n\t#{iState}"
-iFirewall.each do |i|
-  puts "\n\tfrom: #{i.from_port}\n\tto: #{i.to_port}\n\tprotocol:\n\t#{i.protocol}\n\tfrom: #{i.access_from}\n"
+  end
 end
+
+lightsail = AWSSetup.new
+lightsailStatus = LightsailStatus.new
+
+lightsailStatus.getStatus#(lightsail)
+lightsailFirewall = UpdateFirewall.new
+lightsailFirewall.updateFirewall
+lightsailStatus.getStatus
+
+
+# resp = client.get_instances()
+# iFirewall = resp.instances[0].networking.ports
+#
+# puts "#{iName}\n\t#{iPublicIP}\n\t#{iDateCreated}\n\t#{iState}"
+# iFirewall.each do |i|
+#   puts "\n\tfrom: #{i.from_port}\n\tto: #{i.to_port}\n\tprotocol:\n\t#{i.protocol}\n\tfrom: #{i.access_from}\n"
+# end
 
 # resp.values.each do |level|
 #   puts "#{level}\n"
